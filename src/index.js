e6c0a1905d86e927c1bc0c2460597142d047b5ed
gokulrajalp <gokulrajalp@gmail.com>
2022-02-22 20:48:13 +0530

    update

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <h1>Gokul Changed the file</h1>
   </React.StrictMode>,
   document.getElementById('root')
 );
