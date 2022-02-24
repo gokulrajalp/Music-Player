d0661632608d6c42fdd46248455abe6ef48ea1bf
unknown <gokulrajalp@gmail.com>
2022-02-24 20:03:40 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import 'bootstrap/dist/css/bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import Button from 'react-bootstrap/Button'
 ReactDOM.render(
   <React.StrictMode>
