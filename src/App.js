f6363686a8fd9d617be7d9101168af5f0982bd25
RithikaKanagaraj <19cs076rithikak@ksrce.ac.in>
2022-02-25 22:29:13 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import 'bootstrap/dist/js/bootstrap.min.js'
 
 function App() {
   return (
-    <nav class="navbar navbar-expand-lg navbar-light">
+    <div>
+    <nav class="navbar navbar-expand-lg navbar-light ">
   <div class="container-fluid">
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -13,29 +14,49 @@ function App() {
     <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
       
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-        <a class="nav-link active" href="#">All</a>
+        <li class="nav-item" >
+        <a class="nav-link active text-success" href="#" >All</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link active"  href="#">New Songs</a>
+          <a class="nav-link active text-success"  href="#">New Songs</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link active" href="#">Old Songs</a>
+          <a class="nav-link active text-success" href="#">Old Songs</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link active" href="#">Album</a>
+          <a class="nav-link active text-success" href="#">Album</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link active" href="#">My Music</a>
+          <a class="nav-link active text-success" href="#">My Music</a>
         </li>
       </ul>
-      <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-        <button class="btn btn-outline-success" type="submit">Search</button>
+      <form class="d-flex " >
+        <input class="form-control me-2 d-none d-md-inline" type="search" placeholder="Search" aria-label="Search" />
+        <button class="btn btn-outline-success d-none d-md-inline" type="submit">Search</button>
       </form>
     </div>
   </div>
 </nav>
+<div class="row">
+  <div class="col-md-4  bg-primary">
+    <div>
+    {/* mulu mathi,mudhal nee mudivum nee,oru manam,halamithi habibo;jalabulajangu,natpuku oru kovil,yealae yealae dosthu da;en frienda pola,friendship anthem,gala gala galagang;pallikoodam;engo piranthom */}
+    </div>
+ 
+  </div>
+  <div class=" col-md-4 bg-primary">
+<div>this is
+      
+    </div>
+  </div>
+  <div class=" col-md-4  bg-primary">
+  <div>
+  this is
+    </div>
+  </div>
+</div>
+  </div>
+
   );
 }
 
