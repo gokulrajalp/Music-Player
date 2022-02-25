37816c082a3b79194cd7cd7cc2797082df46ee7a
RithikaKanagaraj <19cs076rithikak@ksrce.ac.in>
2022-02-25 21:05:05 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
-// import 'bootstrap/dist/css/bootstrap-theme.min.css'
-// import 'bootstrap/dist/css/bootstrap.min.js'
+import 'bootstrap/dist/css/bootstrap-theme.min.css'
+import 'bootstrap/dist/css/bootstrap.min.js'
 import {Button} from 'react-bootstrap'
 
 function App() {
