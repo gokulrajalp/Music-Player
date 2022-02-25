24f6e07105d95c2f1bb02e77a7ee4020b4c88e6c
gokulrajalp <gokulrajalp@gmail.com>
2022-02-25 21:04:02 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
-// import 'bootstrap/dist/css/bootstrap-theme.min.css'
-// import 'bootstrap/dist/css/bootstrap.min.js'
-import {Button} from 'react-bootstrap'
+import 'bootstrap/dist/js/bootstrap.min.js'
 
 function App() {
   return (
