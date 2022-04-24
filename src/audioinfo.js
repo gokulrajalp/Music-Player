617b15e25f5e1904226193d6a8e1b413131a3f13
gokulrajalp <gokulrajalp@gmail.com>
2022-04-24 20:57:47 +0530

    all songs are added

--- src/audioinfo.js
+++ src/audioinfo.js
@@ -1,5 +1,15 @@
-import mulumathe from './audios/Mulumathy Avalathu Mugaam - Masstamilan.In-.mp3'
-import muthalnee from './audios/Mudhal-Nee-Mudivum-Nee-Title-Track-MassTamilan.io.mp3'
+import mulumathe from './audios/Mulumathy Avalathu Mugaam.mp3'
+import muthalnee from './audios/Mudhal-Nee-Mudivum-Nee.mp3'
+import Oru_Manam from './audios/Oru_Manam.mp3'
+import Arabic_Kuthu from './audios/Arabic_Kuthu.mp3'
+import Two_Two from './audios/Two_Two.mp3'
+import Melala_Vedikudhu from './audios/Melala_Vedikudhu.mp3'
+import Friendship_Anthem from './audios/Friendship_Anthem.mp3'
+import En_Friend from './audios/En_Friend.mp3'
+import Pallikoodam from './audios/Pallikoodam.mp3'
+import Engo_piranthom from './audios/Engo_piranthom.mp3'
+import yealae_yealae from './audios/yealae_yealae.mp3'
+import Gala_Gala from './audios/Gala_Gala.mp3'
 
 
 
@@ -7,7 +17,7 @@ import mulumathi from "./images/1.jpg"
 import muthanee from "./images/muthanee.jpg"
 import orumanam from "./images/orumanam.jpg"
 import halamithi from "./images/halamithi.jpg"
-import penne from "./images/penne.jpg"
+import TwoTwo from "./images/TwoTwo.jpg"
 import natpu from "./images/natpu.jpg"
 import enfriend from "./images/enfriend.jpg"
 import antham from "./images/antham.jpg"
@@ -38,7 +48,8 @@ const info = [
         "songId":2,
         "movie":"Dhruva Natchathiram",
         "Artists":"Harris Jayaraj, Karthik,Shashaa Tirupati, Vikram",
-        "image":orumanam
+        "image":orumanam,
+        "song":Oru_Manam                                                                                        
         
     },
     {
@@ -46,62 +57,71 @@ const info = [
         "songId":3,
         "movie":"Beast",
         "Artists":"Anirudh Ravichander, Jonita Gandhi",
-        "image":halamithi
+        "image":halamithi,
+        "song":Arabic_Kuthu
     },
     {
-        "name":"jalanbula",
+        "name":"Two Two Two",
         "songId":4,
-        "movie":"",
-        "Artists":"",
-        "image":penne
+        "movie":"Kaathuvaakula Rendu Kaadhal",
+        "Artists":"Anirudh Ravichander",
+        "image":TwoTwo,
+        "song":Two_Two
     },
     {
         "name":"Melala Vedikudhu",
         "songId":5,
         "movie":"Arrambam",
         "Artists":"Yuvan Shankar Raja",
-        "image":natpu
+        "image":natpu,
+        "song":Melala_Vedikudhu
     },
     {
         "name":"En Friend Pola",
         "songId":6,
         "movie":"Nanban",
         "Artists":"Krish, Suchith Suresan",
-        "image":enfriend
+        "image":enfriend,
+        "song":En_Friend
     },{
         "name":"Friendship Anthem(From Oh My Kadavule)",
         "songId":7,
         "movie":"Oh My Kadavule",
         "Artists":"Leon James,Anirudh Ravichander,M.M.Manasi",
-        "image":antham
+        "image":antham,
+        "song":Friendship_Anthem
     },
     {
         "name":"pallikoodam-The Farewell Song -From Natpe Thunai",
         "songId":8,
         "movie":"Natpe Thunai",
         "Artists":"Hiphop Tamizha, Sanjith Hegde",
-        "image":pallikudam
+        "image":pallikudam,
+        "song":Pallikoodam
     },
     {
         "name":"Engo piranthom",
         "songId":9,
         "movie":"Pattalam",
         "Artists":"Jassie Gift, Arun Gopa, Shanmom, Thushar, Anupraveen",
-        "image":piranthom
+        "image":piranthom,
+        "song":Engo_piranthom
     },
     {
         "name":"yealae yealae Dosthu Da",
         "songId":10,
         "movie":"Endrendrum Punnagai",
         "Artists":"Harris Jayaraj, Krishh,Naresh Iyer, Krishna Iyer",
-        "image":yealae
+        "image":yealae,
+        "song":yealae_yealae
     },
     {
         "name":"Gala Gala",
         "songId":11,
         "movie":"KO",
         "Artists":"Harris Jayaraj, Tippu,Krishh, Harichanran,Sayanora Philip",
-        "image":kalakala
+        "image":kalakala,
+        "song":Gala_Gala
     },
 ]
 
